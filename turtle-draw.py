800ce745c9feef43827a9a59f04861bff1c54d34
[[Brian Lacniak]] <[[BrianLacniak@lewisu.edu]]>
2022-11-04 10:49:14 -0500

    New Python File

--- turtle-draw.py
+++ turtle-draw.py
@@ -1,4 +1,15 @@
+# TurtleDraw
+# Brian Lacniak
+# Lewis University 2022 
+
+import turtle
+
 print("Reading turtle-draw.txt")
+
+turtleScreen = turtle.Screen()
+turtleScreen.setup(450,450)
+
+turtleApp = turtle.Turtle()
 
 txtFileName = "turtle-draw.txt"
 
@@ -9,12 +20,14 @@ while line:
         parts=line.split()
         print(parts)
         if (len (parts) ==3):
-            print("3 parts")
+            print(parts[0])
+            print(int(parts[1]))
+            print(int(parts[2]))
     
     
         if (len (parts) == 1):
             print("1 part")
 
-            
+
         line = file.readline()
 
